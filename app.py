f26ccc9dbe0d2892f66b40c20eb883c5d6b0cda2
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-07-30 03:37:13 -0700

    modified navbar color and size of search

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
+    data = scrape.format_data(scrape.get_images('static/images'))
     return render_template("home.html", title="Foodridise",
-    data=scrape.format_data(scrape.get_images('static/images')))
+    data=data)
 
 
 if __name__ == "__main__":

3b300d76bb9fd3b1a7b04688626a4e1aa0d2f499
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-07-29 22:35:59 -0700

    added data

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+data = [
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        {"name": "Lorum Pizzeria", "src": "https://image.freepik.com/free-photo/pork-boat-noodles-classic-thai-food-popular-menus-ready-eat-soups-there-is-also-basil-bowl_1150-24194.jpg"},
+        ]
+
+@app.route("/")
 def home():
-	return render_template("home.html", title="Foodridise")
+    return render_template("home.html", title="Foodridise", data=data)
 
 
 if __name__ == "__main__":
-	app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)

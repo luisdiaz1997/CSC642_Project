0c6b4266d7fe32b0e9690409145a0c94caefbe30
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-07-31 01:26:20 -0700

    saved fake data to csv

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
+import json
 import scrape
+import pandas as pd
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    data = scrape.format_data(scrape.get_images('static/images'))
+
+    try:
+        df = pd.read_csv('static/data.csv')
+    except:
+        df = scrape.construct_data('static/images')
+        df.to_csv('static/data.csv', index=False)
+
+    df_json = df.to_json(orient='records')
+
     return render_template("home.html", title="Foodridise",
-    data=data)
+    data=json.loads(df_json))
 
 
 if __name__ == "__main__":

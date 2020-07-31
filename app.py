1661d27e1199c62be2434c82019523c71f4cc783
Kevin Venegas <kevinv234@gmail.com>
2020-07-31 01:25:21 -0700

    Added the routes needed for login, registration, and place_order. Going to merge branches in order to start linking templates

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 from flask import Flask, render_template
 import scrape
+
+#importing flask into app
 app = Flask(__name__)
 
-
+#route for homepage
 @app.route("/")
 def home():
     data = scrape.format_data(scrape.get_images('static/images'))
     return render_template("home.html", title="Foodridise",
     data=data)
+
+#route for login 
+@app.route("/login")
+def login():
+    return render_template("starter.html", title="Login")
+
+#route for registration
+@app.route("/register")
+def register():
+    return render_template("starter.html", title = "Registration")
+    
+
+#route for registration
+@app.route("/place_order")
+def order():
+    return render_template("starter.html", title = "Place Order")
+
+
+
+
 
 
 if __name__ == "__main__":

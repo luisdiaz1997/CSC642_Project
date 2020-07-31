47f7cd5a3dc0bce8398e00a1d4bf3c587437096f
Kevin Venegas <kevinv234@gmail.com>
2020-07-31 02:42:29 -0700

    Created endpoints for registration, setting flags for if users have an account alread or if they are creating a specific account with their own restaurant.

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
 import json
 import scrape
-
 import pandas as pd
-app = Flask(__name__)
-
 
 #importing flask into app
 app = Flask(__name__)
@@ -34,10 +31,29 @@ def login():
 #route for registration
 @app.route("/register")
 def register():
-    return render_template("starter.html", title = "Registration")
-    
 
-#route for registration
+    #if account exist, send to login
+    userExists = False
+    if userExists:
+        redirect("/login")
+
+    return render_template("register.html", title = "Registration")
+
+   
+#route for registering ones restaurant
+#might need to append endpoint to register 
+#TODO indicator on register if individual is making a account with a restaurant as well
+#TODO once someone enters resteraunt take them directly to menu route
+@app.route("/restaurant_reg")
+def resteraunt_reg():
+    return render_template("restaurant_reg.html", title = "Register you Resteraunt!")
+
+#route for restaurant to input menu images
+@app.route
+def menu_input():
+    return render_template("menu_input.html", title = "Menu Input")
+
+#route for placing order 
 @app.route("/place_order")
 def order():
     return render_template("starter.html", title = "Place Order")

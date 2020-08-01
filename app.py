288c4b539796301e3034252286eb0c861bacadde
gianclbal <gbaldonado@mail.sfsu.edu>
2020-07-31 20:20:02 -0700

    worked on lorem pizzeria page

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ def home():
     return render_template("home.html", title="Foodridise",
     data=data)
 
+@app.route("/lorempizzeria")
+def lorempizzeria():
+    data = scrape.format_data(scrape.get_images('static/images2'))
+    return render_template("lorempizzeria.html", title="Lorem Pizzeria",
+    data=data)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
